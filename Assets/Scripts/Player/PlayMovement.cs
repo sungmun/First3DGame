b0fbbdfeaad5c3f11d1af8807aaf16b5468894d3
seungmun <tjdans174@gmail.com>
2018-07-02 17:44:01 +0900

    Update : add Update Method Contents to PlayMovement C# Script

--- Assets/Scripts/Player/PlayMovement.cs
+++ Assets/Scripts/Player/PlayMovement.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class PlayMovement : MonoBehaviour {
 
-    protected Animation avatar;
+    protected Animator avatar;
 
     float lastAttackTime, lastSkillTime, lastDashTime;
     public bool attacking = false;
     public bool dashing = false;
 
     float h, v;
-	// Use this for initialization
+	
 	void Start () {
-        avatar = GetComponent<Animation>();
+        avatar = GetComponent<Animator>();
 	}
 
     public void OnStickChanged(Vector2 stickPos){
         h = stickPos.x;
         v = stickPos.y;
     }
-	// Update is called once per frame
+	
 	void Update () {
-		
+        if (avatar) {
+            float back = 1f;
+
+            if (v < 0f) back = -1f;
+
+            avatar.SetFloat("Speed", (h * h + h * v));
+
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            if (rigidbody) {
+                Vector3 speed = rigidbody.velocity;
+                speed.x = 4 * h;
+                speed.y = 4 * v;
+
+                rigidbody.velocity = speed;
+                if(h!=0f && v != 0f) {
+                    transform.rotation = Quaternion.LookRotation(new Vector3(h, 0f, v));
+                }
+            }
+        }
 	}
+
 }
