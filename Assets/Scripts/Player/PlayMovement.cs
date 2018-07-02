da86019ef53639a58bebab6d55e0fe999fdfbf15
seungmun <tjdans174@gmail.com>
2018-07-02 17:43:02 +0900

    Update : add OnStickChanged Method to PlayMovement C# Script

--- Assets/Scripts/Player/PlayMovement.cs
+++ Assets/Scripts/Player/PlayMovement.cs
@@ -10,11 +10,16 @@ public class PlayMovement : MonoBehaviour {
     public bool attacking = false;
     public bool dashing = false;
 
+    float h, v;
 	// Use this for initialization
 	void Start () {
         avatar = GetComponent<Animation>();
 	}
-	
+
+    public void OnStickChanged(Vector2 stickPos){
+        h = stickPos.x;
+        v = stickPos.y;
+    }
 	// Update is called once per frame
 	void Update () {
 		
