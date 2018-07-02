1454df1c3eeb472af0a6272f89b7c121f2a98e6f
seungmun <tjdans174@gmail.com>
2018-07-02 17:29:06 +0900

    Update : add Start Method Content to PlayMovement C# Script

--- Assets/Scripts/Player/PlayMovement.cs
+++ Assets/Scripts/Player/PlayMovement.cs
@@ -12,7 +12,7 @@ public class PlayMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        avatar = GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
