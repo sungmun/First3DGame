e7a07997ee7edecc8eb0905784f2ac13c163cd53
seungmun <tjdans174@gmail.com>
2018-07-03 21:56:52 +0900

    Update : add Fields to EnemyMove C# Script

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour {
 
-	// Use this for initialization
+    Transform player;
+    NavMeshAgent nav;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
