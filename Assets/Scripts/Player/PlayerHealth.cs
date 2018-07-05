4f808d8623f31a0e7d47dca43c94074c1b31c098
seungmun <tjdans174@gmail.com>
2018-07-05 15:02:08 +0900

    Update : add fields remark to PlayerHealth C# script

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,32 +5,34 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
-    public int startingHealth = 100;
-    public int currentHealth;
+    public int startingHealth = 100;    //플레이어의 총체력
+    public int currentHealth;           //플레이어의 현재체력
 
-    public float flashSpeed = 5f;
+    public float flashSpeed = 5f;       //데미지를 입을 경우 화면을 물들이고 있을 시간
 
-    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
-    public Slider healthSlider;
-    public Image damageImage;
-    public AudioClip deathClip;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);  //데미지를 입을 경우 화면을 물들일 색깔
+    public Slider healthSlider; //HealthUI의 Slider객체 추가
+    public Image damageImage;   //damage를 받을경우 화면에 출력할 이미지 추가
+    public AudioClip deathClip; 
 
-    Animator anim;
-    AudioSource playerAudio;
-    PlayMovement playMovement;
+    Animator anim;              //PlayerAnimator 추가
+    AudioSource playerAudio;    
+    PlayMovement playMovement;  //PlayMovement C# 스크립트 
 
-    bool isDead;
-    bool damaged;
+    bool isDead;            //죽었는지 여부 확인
+    bool damaged;           //데미지를 입었는지 현제 상태의 여부 확인
 
     private void Awake() {
         anim = GetComponent<Animator>();
         playerAudio = GetComponent<AudioSource>();
         playMovement = GetComponent<PlayMovement>();
+
         currentHealth = startingHealth;
     }
 
     private void Update() {
         damageImage.color= (damaged)? flashColor: Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+       
         damaged = false;
     }
 
