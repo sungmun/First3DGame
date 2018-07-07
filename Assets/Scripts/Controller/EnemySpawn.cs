ea633270fa55f1cd3f4b5cb1723d10f7a9314b48
seungmun <tjdans174@gmail.com>
2018-07-07 15:50:30 +0900

    Update : add Spawn Method remark to EnemySpawn C# script

--- Assets/Scripts/Controller/EnemySpawn.cs
+++ Assets/Scripts/Controller/EnemySpawn.cs
@@ -14,7 +14,9 @@ public class EnemySpawn : MonoBehaviour {
 	}
 	
     void Spawn() {
-        int spawnPoolIndex = Random.Range(0, spawnPools.Length);
+        int spawnPoolIndex = Random.Range(0, spawnPools.Length); //0부터 spawnPools.Length까지의 값중에 랜덤한 값을 호출
         Instantiate(enemy, spawnPools[spawnPoolIndex].position, spawnPools[spawnPoolIndex].rotation);
+        //Instantiate는 유니티에 오브젝트를 추가를 해주는 메소드이다.
+        //첫번째 인자값은 추가를 할 오브젝트이며, 두번째 인자값은 추가할 오브젝트의 위치, 세번째 인자값은 추가할 오브젝트의 회전정도를 나타낸다.
     }
 }
